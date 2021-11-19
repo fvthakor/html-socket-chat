1152f3898539ee1d074add392d74e23236aec540
Fv Thakor <fvthakor11@gmail.com>
2021-11-19 22:25:08 +0530

    update background

--- app.js
+++ app.js
@@ -20,6 +20,6 @@ io.on('connection', (socket) => {
     });
 });
 
-server.listen(process.env.PORT || 5000, () => {
+server.listen(process.env.PORT || 3000, () => {
   console.log('listening on:3000');
 });
